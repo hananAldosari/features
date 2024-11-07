c2cc002551919ebe4acfacd31ad49d5d73c5a935
Majd Alghamdi <majd.alghamdi@globant.com>
2024-11-07 11:06:57 +0300

    Local notification added

--- lib/views/notification_view.dart
+++ lib/views/notification_view.dart
@@ -17,20 +17,55 @@ class _NotificationViewState extends State<NotificationView> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            print("Entered");
-            DateTime data = DateTime.now().add(const Duration(seconds: 1));
-            
-            await localNotificationService.scheduledNotification(
-              title: "Alarm",
-              body: "Reminder",
-              scheduledTime: data,
-            );
-            print("Finished");
-
-          },
-          child: const Text("Press to schedule"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InkWell(
+              onTap: () async {
+                print("Entered");
+                DateTime data = DateTime.now().add(const Duration(seconds: 1));
+                await localNotificationService.scheduledNotification(
+                  title: "Glob Notification",
+                  body: "Don't forget your from Globant Flutter developers",
+                  scheduledTime: data,
+                );
+                print("Finished");
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 221, 255, 194),
+                  borderRadius: BorderRadius.circular(5),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.shade300,
+                      blurRadius: 7,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  "Press see the scheduled notification",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 84, 84, 84),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              "This notification will be shown in 1 second",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ],
         ),
       ),
     );
